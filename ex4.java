763fd587e0230968e8c0a42dffaa608d1e595efa
SoniaDias <sonia.c.a.dias@gmail.com>
2021-01-29 12:26:23 +0000

    Add files via upload

---
+++ ex4.java
@@ -0,0 +1,36 @@
+package com.mycompany.pocs;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        try 
+        {
+            
+            HelloWorld hw = new HelloWorld();
+
+            Method printItMethod = HelloWorld.class.getMethod("printIt");
+            printItMethod.invoke(hw, null);
+        } 
+        catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) 
+        {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+        
+}
+
+class HelloWorld
+{
+    public void printIt()
+    {
+        System.out.println("Hello World");
+	String password = "cenas";
+    }
+}
